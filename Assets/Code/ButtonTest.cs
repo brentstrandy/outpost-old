9801653867092d78ad15b78909c283a545bfd6ca
John Fitzgerald <wobey1@gmail.com>
2015-01-19 20:59:09 -0800

    Added main menu UI and reorganized file hierarchy to only contain one Assets folder

---
+++ Assets/Code/ButtonTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTest : MonoBehaviour 
+{
+    Canvas can_0;
+
+    public void ButtonPress()
+    {
+            Debug.Log("Button Press");
+    }
+
+    public void ButtonPress(string buttonName)
+    {
+        Debug.Log(buttonName);
+    }
+
+    public void SwitchCamera(string canvasName)
+    {
+        //Canvas can_all = GameObject.Find("Canvas") as Canvas;
+        
+        can_0 = GetComponent("Canvas (All)/Canvas0") as Canvas;
+        //can_0 = GetComponent<Canvas>();
+        //Canvas can_1 = GetComponent(canvasName) as Canvas;
+
+        //if (can_all)
+        //{
+            
+
+        //    //can_0.enabled = false;
+        //    //can_1.enabled = true;
+        //}
+        //else
+        //    Debug.LogError("Cannot find Canvas in scene.");
+    }
+}
