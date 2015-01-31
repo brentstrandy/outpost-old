4fac0cc7a123bf8910bc777e8847a8fe09837565
John Fitzgerald <wobey1@gmail.com>
2015-01-30 21:04:50 -0800

    Fleshed out more of the menu UI within "MenuUI" scene, and buttons play Jaime's clicks

--- Assets/Code/GUIMenuManager.cs
+++ Assets/Code/GUIMenuManager.cs
@@ -5,102 +5,136 @@ using System.Collections.Generic;
 public class GUIMenuManager : MonoBehaviour 
 {
 	public bool ShowDebugLogs = true;
-
-    List<GameObject> Canvases;
-    List<GameObject> Panels;
-    List<GameObject> MenuHeaders;
-    List<GameObject> Buttons;
-
-    string CurrentMenu;
-    string PreviousMenu;
-
-    public AudioClip AudioClick0, AudioClick1;
+    public List<GameObject> Canvases, Panels, MenuHeaders, Buttons;
+    [HideInInspector]
+    public string CurrentMenu, PreviousMenu;
+    [HideInInspector]
+    public GameObject CurrentMenu_GO, PreviousMenu_GO;
+    [HideInInspector]
+    public AudioClip ShortClick, LongClick;
 
     void Start()
     {
-        CurrentMenu = "Main Menu Panel";
+        CurrentMenu = "Main Menu";
 
         #region Lists of GUI Menu elements
-        Canvases = new List<GameObject>(GameObject.FindGameObjectsWithTag("Canvas"));
-        Panels = new List<GameObject>(GameObject.FindGameObjectsWithTag("Panel"));
-        MenuHeaders = new List<GameObject>(GameObject.FindGameObjectsWithTag("Menu Header"));
-        Buttons = new List<GameObject>(GameObject.FindGameObjectsWithTag("Button"));
+        //Panels = new List<GameObject>(GameObject.FindGameObjectsWithTag("Panel"));
+        //// storing these just in case we need customization later on
+        //Canvases = new List<GameObject>(GameObject.FindGameObjectsWithTag("Canvas"));
+        //MenuHeaders = new List<GameObject>(GameObject.FindGameObjectsWithTag("Menu Header"));
+        //Buttons = new List<GameObject>(GameObject.FindGameObjectsWithTag("Button"));
 
-        if (ShowDebugLogs)
-        {
-            foreach (GameObject canvas in Canvases)
-                Debug.Log("  [CANVAS]" + canvas.name);
-            foreach (GameObject panel in Panels)
-                Debug.Log("  [PANEL] " + panel.name);
-            foreach (GameObject text in MenuHeaders)
-                Debug.Log("  [MENU HEADER] " + text.name);
-            foreach (GameObject button in Buttons)
-                Debug.Log("  [BUTTON] " + button.name);
-        }
+        //// disables all panels except "CurrentMenu"
+        //foreach (GameObject panel in Panels)
+        //{
+        //    if (panel.name != CurrentMenu)
+        //    {
+        //        panel.SetActive(false);
+        //        panel.layer = LayerMask.NameToLayer("UI"); // was "Initial layer"
+        //    }
+        //}
+
+        //CurrentMenu_GO = Panels.Find(panel => panel.name == CurrentMenu);
+
+        //if (ShowDebugLogs)
+        //{
+        //    foreach (GameObject canvas in Canvases)
+        //        this.Log("  [CANVAS]" + canvas.name);
+        //    foreach (GameObject panel in Panels)
+        //        this.Log("  [PANEL] " + panel.name);
+        //    foreach (GameObject header in MenuHeaders)
+        //        this.Log("  [MENU HEADER] " + header.name);
+        //    foreach (GameObject button in Buttons)
+        //        this.Log("  [BUTTON] " + button.name);
+        //}
         #endregion
 
-        Debug.LogError(Application.dataPath);
-
-        AudioClick0 = Resources.Load("/Sounds/Effects/ui_click_01.wav") as AudioClip;
-        AudioClick1 = Resources.Load("/Sounds/Effects/ui_click_02.wav") as AudioClip;
+        #region Sound Effects
+        string soundEffectsPath = "Audio/Effects/";
+        ShortClick = Resources.Load(soundEffectsPath + "short_click_01") as AudioClip;
+        LongClick = Resources.Load(soundEffectsPath + "long_click_01") as AudioClip;
+        #endregion
     }
 
-    void Update()
+    #region USED WITH LEGACY GUI SCENE
+    //public void ButtonClick(string menuName)
+    //{
+    //    //ButtonSound();
+    //    MenuChange(menuName);
+    //}
+
+    //public void ButtonSound(string menuName)
+    //{
+    //    // create system for playing a short or long click ("simple selection" vs "critical selection")
+    //    if (true)
+    //        PlayOneShotSound(ShortClick);
+    //    else
+    //        PlayOneShotSound(LongClick);
+    //}
+
+    //public void PlayOneShotSound(AudioClip clip)
+    //{
+    //    if (clip)
+    //        audio.PlayOneShot(clip);
+    //    else
+    //        this.LogError("Audio clip not available.");
+    //}
+    #endregion
+
+    public void PlayShortClick()
     {
-        //// make main menu reappear
-        //if (Input.GetKeyDown("space"))
-        //{
-        //    foreach (GameObject button in Buttons)
-        //    {
-        //        if (button.name == "Create Game")
-        //        {
-        //            button.transform.parent.gameObject.SetActive(true);
-        //        }
-        //    }
-        //}
+        audio.PlayOneShot(ShortClick);
     }
 
-    public void ButtonPress()
+    public void PlayLongClick()
     {
-		Debug.Log("Button Press");
+        audio.PlayOneShot(LongClick);
     }
 
-    public void ButtonPress(string buttonName)
+    //public void MenuChange(string menuName)
+    //{
+    //    string tempCurrentMenu = CurrentMenu;
+    //    GameObject tempCurrentMenu_GO;
+
+    //    //if (menuName == "Create Game")
+    //    //{
+    //    //    Application.LoadLevel("MainGame");
+    //    //}
+
+    //    if (menuName != "Back")
+    //    {
+    //        // if the next menu item (menuName) DOES exist...
+    //        if ((tempCurrentMenu_GO = Panels.Find(panel => panel.name == menuName)) != null)
+    //        {
+    //            // set the previous menu
+    //            PreviousMenu_GO = CurrentMenu_GO;
+    //            PreviousMenu = CurrentMenu;
+    //            // set the current menu
+    //            CurrentMenu_GO = tempCurrentMenu_GO;
+    //            CurrentMenu = menuName;
+
+    //            PreviousMenu_GO.SetActive(false);
+    //            CurrentMenu_GO.SetActive(true);
+    //        }
+    //        // if the next menu item (menuName) DOES NOT exist...
+    //        else
+    //        {
+    //            this.LogError(menuName + " not available.");
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("         Current Menu: " + CurrentMenu_GO.name);
+    //        CurrentMenu = PreviousMenu;
+    //        CurrentMenu_GO.SetActive(false);
+    //        PreviousMenu_GO.SetActive(true);
+    //    }
+    //}
+
+
+    public void CreateGame()
     {
-        Debug.Log(buttonName);
-    }
-
-	public void CreateGame()
-	{
-		SessionManager.Instance.CreateRoom();
-	}
-
-    public void SwitchMenu(string name)
-    {
-        if (true)
-        {
-            ButtonSound(AudioClick0);
-        }
-        //foreach (GameObject button in Buttons)
-        //{
-        //    if (button.name == name)
-        //    {
-        //        button.transform.parent.gameObject.SetActive(false);
-        //        //button.SetActive(false);
-        //        return;
-        //    }
-        //}
-
-    }
-
-    public void ButtonSound(AudioClip type)
-    {
-        Debug.Log(type.name);
-        if (type)
-        {
-            Debug.Log("  *BOOP NOISE*");
-            audio.PlayOneShot(type);
-        }
+        SessionManager.Instance.CreateRoom();
     }
 
 	/// <summary>
