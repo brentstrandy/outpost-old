5c745b80906250801720ad795a997d89618c987f
Brent Strandy <brentstrandy@gmail.com>
2015-06-03 21:15:30 -0700

    Added FMOD script to play a single audio clip (PlayOneShot) when an object/Prefab is instantiated

---
+++ Assets/Code/Utilities/FMOD_OneShot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FMOD_OneShot : MonoBehaviour
+{
+	public string FMODEventName = "";
+	public Vector3 Position;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		//if(Position == )
+			Position = this.transform.position;
+
+		FMOD_StudioSystem.instance.PlayOneShot("event:/" + FMODEventName, Position);
+	}
+}
