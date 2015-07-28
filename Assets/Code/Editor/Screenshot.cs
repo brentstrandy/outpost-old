ebbbcb61399db6f3b832fda553d14209455758aa
Brent Strandy <brentstrandy@gmail.com>
2015-07-28 10:51:30 -0700

    Added "Take Screenshot" button to all camera objects in the editor. We can now take high resolution screenshots of the game.

---
+++ Assets/Code/Editor/Screenshot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[ExecuteInEditMode]
+[CustomEditor(typeof(Camera))]
+public class Screenshot : Editor
+{
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+	public override void OnInspectorGUI()
+	{
+		if (GUILayout.Button("Take Screenshot"))
+		{
+			Debug.Log("Screenshot Taken. Located in 'Screenshots' folder of Outpost (not visible in Unity)");
+			Application.CaptureScreenshot("Screenshots/screenshot.png", 4);
+		}
+	}
+}
