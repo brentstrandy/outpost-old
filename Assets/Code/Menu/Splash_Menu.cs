0a73f7f3b2abf51ecd5b2910d16fd66858b8268b
Brent Strandy <brentstrandy@gmail.com>
2015-10-24 16:28:10 -0700

    Added rough draft of Diadem/Outpost Splash screen

--- Assets/Code/Menu/Splash_Menu.cs
+++ Assets/Code/Menu/Splash_Menu.cs
@@ -3,32 +3,13 @@
 public class Splash_Menu : MonoBehaviour
 {
     public bool ShowDebugLogs = false;
-    public float DisplayTime = 1.5f;
 
-    private float StartTime = 0;
-    private bool Active = false;
-
-    private void OnEnable()
-    {
-        Active = true;
-        StartTime = Time.time;
-    }
-
-    private void OnDisable()
-    {
-        Active = false;
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        if (Active)
-            if (Time.time - StartTime > DisplayTime)
-                MenuManager.Instance.ShowStartMenu();
-    }
+	public void SplashScreenEnd()
+	{
+		MenuManager.Instance.ShowStartMenu();
+	}
 
     #region MessageHandling
-
     protected void Log(string message)
     {
         if (ShowDebugLogs)
@@ -39,6 +20,5 @@ public class Splash_Menu : MonoBehaviour
     {
         Debug.LogError("[Splash_Menu] " + message);
     }
-
     #endregion MessageHandling
 }
