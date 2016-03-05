c23a79fd4833ae41b0af6e4ae6db5cc3ff08a5e2
Brent Strandy <brentstrandy@gmail.com>
2016-03-05 00:51:03 -0800

    Adding Hammer Tower animations

--- Assets/Code/Towers/HammerTower.cs
+++ Assets/Code/Towers/HammerTower.cs
@@ -9,7 +9,7 @@ public class HammerTower : Tower
 
         //EnemyCircleCollider = this.GetComponent<SphereCollider>();
 
-        // Small Thraceium Tower will fire at enemies, start a coroutine to check (and fire) on enemies
+        // Hammer Tower will attack any enemy within its range, start a coroutine to check (and fire) on enemies
         StartCoroutine("Fire");
 
         //EnemyCircleCollider.radius = TowerAttributes.Range;
@@ -22,33 +22,6 @@ public class HammerTower : Tower
     }
 
     #region Special Effects
-
-    public override void InstantiateFire()
-    {
-        // Instantiate prefab for firing a shot
-        if (FiringEffect)
-        {
-            /*GameObject effect = Instantiate(FiringEffect, EmissionPoint.transform.position, EmissionPoint.transform.rotation) as GameObject;
-
-            // TO DO: Instead of instantiating a new prefab all the time just use one prefab and reset it after each use
-            // Set the color of the laser effect
-            effect.GetComponent<Light>().color = PlayerColor;
-            effect.GetComponent<LineRenderer>().material.SetColor("_Color", PlayerColor);
-            effect.GetComponent<LineRenderer>().material.SetColor("_EmissionColor", PlayerColor);*/
-        }
-    }
-
-	public override void InstantiateExplosion()
-	{
-		if (ExplodingEffect)
-		{
-			GameObject effect = Instantiate(ExplodingEffect, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 1.32f), Quaternion.Euler(0, 180, 0)) as GameObject;
-
-			effect.GetComponentInChildren<Light>().color = PlayerColor;
-			effect.GetComponent<ParticleSystemRenderer>().material.SetColor("_Color", PlayerColor);
-			effect.GetComponent<ParticleSystemRenderer>().material.SetColor("_EmmissionColor", PlayerColor);
-		}
-	}
 
     #endregion Special Effects
 
